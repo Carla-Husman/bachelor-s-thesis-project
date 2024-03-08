139afbaebd7ab37d4c53f6c3a9bbc35c97997d25
Carla-Husman <125917152+Husman-Carla@users.noreply.github.com>
2024-03-08 12:00:15 +0200

    [FRONTEND] - Add navbar and footer

--- Angular-Project/src/app/app.component.ts
+++ Angular-Project/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterOutlet} from '@angular/router';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {DBConfig, NgxIndexedDBModule} from 'ngx-indexed-db';
 import {EncrDecrService} from "./services/EncrDecrService/encr-decr.service";
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatTabLink} from "@angular/material/tabs";
 
 @Component({
   selector: 'app-root',
@@ -14,11 +17,23 @@ import {EncrDecrService} from "./services/EncrDecrService/encr-decr.service";
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatToolbar,
+    MatToolbarModule,
+    MatIcon,
+    MatTabLink
   ],
   providers: [EncrDecrService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'RelationTrip';
+  navBarActive = false;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.navBarActive = localStorage.getItem('username') != undefined || localStorage.getItem('username') != null;
+  }
 }
