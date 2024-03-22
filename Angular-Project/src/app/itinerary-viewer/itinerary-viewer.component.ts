7117e5d71aa00e827990265ccf8457ee94366d21
Carla-Husman <125917152+Husman-Carla@users.noreply.github.com>
2024-03-23 01:07:15 +0200

    [FRONTEND] - Add map and markers

--- Angular-Project/src/app/itinerary-viewer/itinerary-viewer.component.ts
+++ Angular-Project/src/app/itinerary-viewer/itinerary-viewer.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
@@ -8,6 +8,7 @@ import {db} from "../db";
 import {NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {MatListModule} from "@angular/material/list";
 import {OverlayModule} from "@angular/cdk/overlay";
+import {GoogleMap, GoogleMapsModule} from "@angular/google-maps";
 
 @Component({
   selector: 'app-itinerary-viewer',
@@ -23,7 +24,8 @@ import {OverlayModule} from "@angular/cdk/overlay";
     MatIconModule,
     NgForOf,
     NgIf,
-    OverlayModule
+    OverlayModule,
+    GoogleMapsModule
   ],
   templateUrl: './itinerary-viewer.component.html',
   styleUrl: './itinerary-viewer.component.css'
@@ -32,121 +34,24 @@ import {OverlayModule} from "@angular/cdk/overlay";
 export class ItineraryViewerComponent implements OnInit {
   itineraries: any
   stars: number[] = [];
-  isOpened = false;
   isOpenSchedule: boolean[] = [];
   isOpenPhone: boolean[] = []
   isOpenWebsite: boolean[] = []
   isOpenAddress: boolean[] = []
 
-  async add() {
-    await db.itineraries.add({
-      id: 0,
-      userId: 1,
-      "photo": "assets/images/california-tour.jpg",
-      "name": "California Tour",
-      "destination": "California",
-      "season": null,
-      "attendant": null,
-      "distance": 0,
-      "poisNumber": 5,
-      "highlights": [
-        "Explore the vibrant art scene in California",
-        "Immerse yourself in the breathtaking natural landscapes",
-        "Embark on exhilarating hiking adventures"
-      ],
-      "pois": [
-        {
-          "name": "Yosemite National Park",
-          "photo": "assets/images/california-tour.jpg",
-          "description": "Yosemite National Park is renowned for its granite cliffs, waterfalls, clear streams, and giant sequoias. It offers a perfect blend of nature and hiking opportunities.",
-          "longitude": -119.5383294,
-          "latitude": 37.8651011,
-          "tags": ["nature", "hiking"],
-          "stars": 4.8,
-          "address": "California, United States",
-          "phone": "+1 209-372-0200",
-          "website": "https://www.nps.gov/yose/index.htm",
-          "schedule": [
-            "Monday: Open 24 hours",
-            "Tuesday: Open 24 hours",
-            "Wednesday: Open 24 hours",
-            "Thursday: Open 24 hours",
-            "Friday: Open 24 hours",
-            "Saturday: Open 24 hours",
-            "Sunday: Open 24 hours"
-          ]
-        },
-        {
-          "name": "The Getty",
-          "photo": "assets/images/california-tour.jpg",
-          "description": "The Getty Center is a renowned art museum and architectural masterpiece, featuring an impressive collection of European paintings, sculptures, and decorative arts.",
-          "longitude": -118.4740954,
-          "latitude": 34.07803579999999,
-          "tags": ["art", "nature"],
-          "stars": 4.8,
-          "address": "1200 Getty Center Dr, Los Angeles, CA 90049, United States",
-          "phone": "+1 310-440-7300",
-          "website": "https://www.getty.edu/visit/center/",
-          "schedule": [
-            "Monday: Closed",
-            "Tuesday: 10:00 AM – 5:30 PM",
-            "Wednesday: 10:00 AM – 5:30 PM",
-            "Thursday: 10:00 AM – 5:30 PM",
-            "Friday: 10:00 AM – 5:30 PM",
-            "Saturday: 10:00 AM – 8:00 PM",
-            "Sunday: 10:00 AM – 5:30 PM"
-          ]
-        },
-        {
-          "name": "Big Sur",
-          "photo": "assets/images/california-tour.jpg",
-          "description": "Big Sur is a rugged stretch of California's central coast, known for its dramatic cliffs, stunning ocean views, and numerous hiking trails that lead to hidden coves and waterfalls.",
-          "longitude": -121.8080556,
-          "latitude": 36.2704233,
-          "tags": ["nature", "hiking"],
-          "stars": -1,
-          "address": "Big Sur, CA, USA",
-          "phone": "",
-          "website": "",
-          "schedule": []
-        },
-        {
-          "name": "The Broad",
-          "photo": "assets/images/california-tour.jpg",
-          "description": "The Broad is a contemporary art museum in downtown Los Angeles, showcasing an extensive collection of modern and contemporary art, including works by renowned artists.",
-          "longitude": -118.2501802,
-          "latitude": 34.0545021,
-          "tags": ["art"],
-          "stars": 4.7,
-          "address": "221 S Grand Ave, Los Angeles, CA 90012, United States",
-          "phone": "+1 213-232-6200",
-          "website": "https://www.thebroad.org/",
-          "schedule": [
-            "Monday: Closed",
-            "Tuesday: 11:00 AM – 5:00 PM",
-            "Wednesday: 11:00 AM – 5:00 PM",
-            "Thursday: 11:00 AM – 8:00 PM",
-            "Friday: 11:00 AM – 5:00 PM",
-            "Saturday: 10:00 AM – 6:00 PM",
-            "Sunday: 10:00 AM – 6:00 PM"
-          ]
-        },
-        {
-          "name": "Lake Tahoe",
-          "photo": "assets/images/california-tour.jpg",
-          "description": "Lake Tahoe is a picturesque alpine lake surrounded by snow-capped peaks, offering a paradise for nature lovers and hikers with its crystal-clear waters and scenic trails.",
-          "longitude": -120.0323507,
-          "latitude": 39.0968493,
-          "tags": ["nature", "hiking"],
-          "stars": 4.8,
-          "address": "Lake Tahoe, United States",
-          "phone": "",
-          "website": "",
-          "schedule": []
-        }
-      ]
-    })
-  }
+  @ViewChild('myGoogleMap', {static: true}) map!: GoogleMap;
+  mapOptions: google.maps.MapOptions = {};
+  markers: ({
+    options: { animation: google.maps.Animation };
+    position: { lat: number; lng: number };
+    label: { color: string; text: string };
+    title: string
+  } | {
+    options: { animation: null };
+    position: { lat: number; lng: number };
+    label: { color: string; text: string };
+    title: string
+  })[] = [];
 
   async ngOnInit() {
     this.stars = Array(5).fill(0).map((_, i) => i + 1);
@@ -164,10 +69,109 @@ export class ItineraryViewerComponent implements OnInit {
     this.isOpenSchedule = this.itineraries.pois.map(() => false);
     this.isOpenAddress = this.itineraries.pois.map(() => false);
 
-    console.log(this.itineraries)
+    for (let poi of this.itineraries.pois) {
+      this.markers.push({
+        position: {
+          lat: poi.latitude,
+          lng: poi.longitude
+        },
+        title: poi.name,
+        label: {
+          color: 'black',
+          text: (this.markers.length + 1).toString(),
+        },
+        options: {
+          animation: google.maps.Animation.DROP
+        }
+      })
+    }
+
+    this.calculateMapCenterAndZoom()
   }
 
-  saveItinerary() {
-    // save in database
+  calculateMapCenterAndZoom() {
+    if (this.markers.length === 0) {
+      return;
+    }
+
+    // find min and max
+    let minLat = this.markers[0].position.lat;
+    let maxLat = this.markers[0].position.lat;
+    let minLng = this.markers[0].position.lng;
+    let maxLng = this.markers[0].position.lng;
+
+    this.markers.forEach(marker => {
+      minLat = Math.min(minLat, marker.position.lat);
+      maxLat = Math.max(maxLat, marker.position.lat);
+      minLng = Math.min(minLng, marker.position.lng);
+      maxLng = Math.max(maxLng, marker.position.lng);
+    });
+
+    // average of lat and log
+    const centerLat = (minLat + maxLat) / 2;
+    const centerLng = (minLng + maxLng) / 2 - 5;
+
+    // vertical and horizontal distance between bounds
+    const latDelta = maxLat - minLat;
+    const lngDelta = maxLng - minLng;
+
+    // zoom based on vertical or horizontal distance, taking the max zoom
+    const verticalZoom = Math.floor(Math.log2(180 / latDelta));
+    const horizontalZoom = Math.floor(Math.log2(360 / lngDelta));
+    const zoom = Math.max(verticalZoom, horizontalZoom);
+
+    // set map options
+    this.mapOptions = {
+      center: {
+        lat: centerLat,
+        lng: centerLng
+      },
+      zoom: zoom,
+    };
+  }
+
+  goToMyMarker(poi: any) {
+    this.markers = this.markers.map(m => {
+      if (m.position.lat === poi.latitude && m.position.lng === poi.longitude) {
+        return {
+          ...m,
+          options: {
+            animation: google.maps.Animation.BOUNCE
+          }
+        };
+      } else {
+        return {
+          ...m,
+          options: {
+            animation: null
+          }
+        };
+      }
+    });
+  }
+
+  async saveItinerary() {
+    let id_user = await db.transaction('r', [db.users], async () => {
+      return db.users.get({username: window.localStorage.getItem('username')});
+    }).catch(error => {
+      console.error("Error occurred while fetching user:", error);
+    });
+
+    if (id_user == undefined) {
+      return;
+    }
+
+    await db.itineraries.add({
+      userId: id_user.id != undefined ? id_user.id : -1,
+      photo: this.itineraries.photo,
+      name: this.itineraries.name,
+      destination: this.itineraries.destination,
+      season: this.itineraries.season,
+      attendant: this.itineraries.attendant,
+      distance: this.itineraries.distance,
+      poisNumber: this.itineraries.poisNumber,
+      highlights: this.itineraries.highlights,
+      pois: this.itineraries.pois
+    })
   }
 }
